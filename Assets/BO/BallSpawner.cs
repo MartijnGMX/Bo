4d4fbd1d6576a265abd6b6150510b4813bc0f05b
martijn <martijn@ivorylake.com>
2018-01-07 15:32:14 +0100

    Added BallSpawner. Bo can be grabbed by holding trigger in one hand, this lets you 'slide' it

---
+++ Assets/BO/BallSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+	public GameObject spawnPrefab;
+
+	public float initialInterval;
+
+	public float currentInterval;
+
+	public Vector3[] spawnPoints;
+
+	public bool keepSpawning = false;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(KeepSpawning());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// spawn a prefab at one of the spawn points
+	void Spawn(){
+		int i = Random.Range(0, spawnPoints.Length);
+		Vector3 pos = spawnPoints[i];
+
+		Instantiate(spawnPrefab, pos, Quaternion.identity);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		//Gizmos.DrawSphere(transform.position, 1f);
+		foreach(Vector3 pos in spawnPoints) {
+			Gizmos.DrawWireSphere(transform.TransformPoint(pos), 0.3f);
+		}
+	}
+	void SpawnAfterInterval(){
+		StartCoroutine(SpawnOnceAfterInterval());
+	}
+	IEnumerator SpawnOnceAfterInterval(){
+		yield return new WaitForSecondsRealtime(currentInterval);
+		Spawn();
+	}
+	IEnumerator KeepSpawning(){
+		while(true) {
+			if(keepSpawning) {
+				Spawn();
+			}
+			yield return new WaitForSecondsRealtime(currentInterval);
+		}
+	}
+}
--- Assets/BO/CenterBetweenControllers.cs
+++ Assets/BO/CenterBetweenControllers.cs
@@ -7,17 +7,34 @@ public class CenterBetweenControllers : MonoBehaviour {
 	public GameObject leftController;
 	public GameObject rightController;
 
+	VRTK_ControllerEvents leftCEvents;
+	VRTK_ControllerEvents rightCEvents;
+
 	public float leftDistFromCenter;
 	public float rightDistFromCenter;
 
 	public bool leftTriggerPressed;
+	public bool rightTriggerPressed;
 
+	public bool inited;
+
+	[Range(0,1f)]
+	public float lerpVal=0.5f;
 	// Use this for initialization
 	void Start () {
+		inited = false;
+	}
+
+	void Setup(){
 		leftController = VRTK_DeviceFinder.GetControllerLeftHand();
 		rightController = VRTK_DeviceFinder.GetControllerRightHand();
-
-
+		if(leftController != null && rightController != null) {
+			leftCEvents = (leftController.GetComponent<VRTK_ControllerEvents>() ? leftController.GetComponent<VRTK_ControllerEvents>() : leftController.GetComponentInParent<VRTK_ControllerEvents>());
+			rightCEvents = (rightController.GetComponent<VRTK_ControllerEvents>() ? rightController.GetComponent<VRTK_ControllerEvents>() : rightController.GetComponentInParent<VRTK_ControllerEvents>());
+			if(leftCEvents != null && rightCEvents != null) {
+				inited = true;
+			}
+		}
 	}
 
 	void OnEnable(){
@@ -25,29 +42,54 @@ public class CenterBetweenControllers : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		if(leftController != null && rightController != null) {
+		if(inited) {
 			Vector3 leftPos = leftController.transform.position;
 			Vector3 rightPos = rightController.transform.position;
+			Vector3 newPos;
 
-			/*VRTK_ControllerEvents leftCEvents = (leftController.GetComponent<VRTK_ControllerEvents>() ? leftController.GetComponent<VRTK_ControllerEvents>() : leftController.GetComponentInParent<VRTK_ControllerEvents>());
-			if(leftCEvents) {
+			if(leftCEvents != null && rightCEvents != null) {
 				leftTriggerPressed = leftCEvents.triggerPressed;
+				rightTriggerPressed = rightCEvents.triggerPressed;
+			} else {
+				Setup();
+			}
+
+			// compute hand distances:
+			if(!leftTriggerPressed && rightTriggerPressed) {
+				// hold in right hand, recompute left distance
+				leftDistFromCenter = (rightPos - leftPos).magnitude - rightDistFromCenter;
+				lerpVal = leftDistFromCenter / (leftDistFromCenter + rightDistFromCenter);
+			} else if(leftTriggerPressed && !rightTriggerPressed) {
+				// hold in left hand, recompute right distance
+				rightDistFromCenter = (rightPos - leftPos).magnitude - leftDistFromCenter;
+				lerpVal = leftDistFromCenter / (leftDistFromCenter + rightDistFromCenter);
+			} else {
+				// recompute both
+				lerpVal = 0.5f;
+				leftDistFromCenter = (rightPos - leftPos).magnitude * 0.5f;
+				rightDistFromCenter = leftDistFromCenter;
+			}
+
+			/*if(leftTriggerPressed && !rightTriggerPressed) {
+				// keep distance to left hand
+				lerpVal = leftDistFromCenter / (leftDistFromCenter + rightDistFromCenter);
+			}
+			if(rightTriggerPressed && !leftTriggerPressed) {
+				// keep distance to right hand
+				lerpVal = rightDistFromCenter / (leftDistFromCenter + rightDistFromCenter);
 			}*/
+
 			// center position
-			//if (leftController.ge
-			Vector3 newPos = Vector3.Lerp(leftPos, rightPos, 0.5f);
+			newPos = Vector3.Lerp(leftPos, rightPos, lerpVal);
 			transform.position = newPos;
 
 			// orient towards one of the controllers, 
 			// use up vector of controller as 'up'
 			transform.rotation = Quaternion.LookRotation(leftPos - rightPos);
 
-			// compute hand distances:
-			leftDistFromCenter = (newPos - leftPos).magnitude;
-			rightDistFromCenter = (newPos - rightPos).magnitude;
+
 		} else {
-			leftController = VRTK_DeviceFinder.GetControllerLeftHand();
-			rightController = VRTK_DeviceFinder.GetControllerRightHand();
+			Setup();
 		}
 	}
 }
